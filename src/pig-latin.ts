bd3cbe481850aba66e4779e2e16d96fa9e8eb423
skip-rat <skip-rat@users.noreply.github.com>
2023-01-04 17:50:00 +0000

    Update pig-latin.ts
    Improved readability of code

--- src/pig-latin.ts
+++ src/pig-latin.ts
@@ -1,7 +1,8 @@
 export function pigLatin(words :string) : string
 {
   return words
-        .split(" ")
+        .split(' ')
         .filter(word => word.length > 1)
-        .map(word => `${word.substring(1)}${word.substring(0,1)}ay`).join(' ');
+        .map(word => `${word.substring(1)}${word.substring(0,1)}ay`)
+        .join(' ');
 }
